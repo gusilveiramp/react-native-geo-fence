ee5f80ae1002bbb99805ddf644530a59d6b18b67
gusilveiramp <gdsmp@hotmail.com>
2017-10-24 15:10:26 -0200

    Update RNGeoFencePackage.java

--- android/src/main/java/com/bmustapha/reactlibrary/RNGeoFencePackage.java
+++ android/src/main/java/com/bmustapha/reactlibrary/RNGeoFencePackage.java
@@ -15,8 +15,7 @@ public class RNGeoFencePackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RNGeoFenceModule(reactContext));
     }
-
-    @Override
+    
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
